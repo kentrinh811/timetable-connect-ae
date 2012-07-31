2c103404713c8703dcd1f94e6c3bfa3083235937
Cameron Fletcher <cameronfletcher92@gmail.com>
2012-07-31 20:21:02 +1000

    Basic structure

--- war/js/main.js
+++ war/js/main.js
@@ -1,1 +1,20 @@
-blank
+function appendResults(text) {
+var results = document.getElementById('results');
+results.appendChild(document.createElement('P'));
+results.appendChild(document.createTextNode(text));
+}
+
+function makeRequest() {
+var request = gapi.client.urlshortener.url.get({
+  'shortUrl': 'http://goo.gl/fbsS'
+});
+request.execute(function(response) {
+  appendResults(response.longUrl);
+});
+}
+
+function load() {
+alert("hello");
+gapi.client.setApiKey('YOUR API KEY');
+gapi.client.load('urlshortener', 'v1', makeRequest);
+}
