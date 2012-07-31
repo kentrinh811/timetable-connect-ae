021bb22212c43d6c3533bc8233efe029a7edb345
petegleeson <peter.gleeson@uqconnect.edu.au>
2012-07-31 21:00:41 +1000

    updated js formatting

--- war/js/main.js
+++ war/js/main.js
@@ -1,20 +1,20 @@
 function appendResults(text) {
-var results = document.getElementById('results');
-results.appendChild(document.createElement('P'));
-results.appendChild(document.createTextNode(text));
+	var results = document.getElementById('results');
+	results.appendChild(document.createElement('P'));
+	results.appendChild(document.createTextNode(text));
 }
 
 function makeRequest() {
-var request = gapi.client.urlshortener.url.get({
-  'shortUrl': 'http://goo.gl/fbsS'
-});
-request.execute(function(response) {
-  appendResults(response.longUrl);
-});
+	var request = gapi.client.urlshortener.url.get({
+		'shortUrl' : 'http://goo.gl/fbsS'
+	});
+	request.execute(function(response) {
+		appendResults(response.longUrl);
+	});
 }
 
 function load() {
-alert("hello");
-gapi.client.setApiKey('YOUR API KEY');
-gapi.client.load('urlshortener', 'v1', makeRequest);
+	alert("hello");
+	gapi.client.setApiKey('YOUR API KEY');
+	gapi.client.load('urlshortener', 'v1', makeRequest);
 }
